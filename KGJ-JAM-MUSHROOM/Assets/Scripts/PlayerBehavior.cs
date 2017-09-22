612ab9b5227e2350cdd72e07851268703f506dce
theMetabug <metabug@gmail.com>
2017-09-22 22:07:01 +0300

    Slightly better controls

--- KGJ-JAM-MUSHROOM/Assets/Scripts/PlayerBehavior.cs
+++ KGJ-JAM-MUSHROOM/Assets/Scripts/PlayerBehavior.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
 
-    public float moveSpeed = 25.0f;
-    public float lerpMultiplier = 0.5f;
+    public float moveSpeed = 7;
+    public float acceleration = 1f;
+    public float friction = 0.5f;
+    public float lerpMultiplier = 0.75f;
     public System.Collections.Generic.List<KeyCode> keyInputs = new System.Collections.Generic.List<KeyCode>();
+    public bool isPressedAction = false;
 
-    private Vector3 dirForce = new Vector3();
+    private Vector3 dirVel = new Vector3();
+    private float m_speed = 0f;
     private bool m_moving = false;
 
     // Use this for initialization
@@ -27,36 +31,56 @@ public class PlayerBehavior : MonoBehaviour {
 
     private void MovementControls()
     {
-        Vector3 dir = new Vector3();
-
         // Movement input
+
+        // up & down
         if (Input.GetKey(keyInputs[0]))
         {
-            dir.z = 1;
+            dirVel.z = 1;
             m_moving = true;
         }
-        if (Input.GetKey(keyInputs[1]))
+        else if (Input.GetKey(keyInputs[2]))
         {
-            dir.x = -1;
+            dirVel.z = -1;
             m_moving = true;
         }
-        if (Input.GetKey(keyInputs[2]))
+        else
         {
-            dir.z = -1;
-            m_moving = true;
+            dirVel.z = Vector3.Lerp(new Vector3(0, 0, dirVel.z), Vector3.zero, Time.deltaTime * 5).z;
+            if (Mathf.Abs(m_speed) < 0.15f)
+            {
+                dirVel.z = 0;
+            }
         }
+        // left & right
         if (Input.GetKey(keyInputs[3]))
         {
-            dir.x = 1;
+            dirVel.x = 1;
             m_moving = true;
         }
+        else if (Input.GetKey(keyInputs[1]))
+        {
+            dirVel.x = -1;
+            m_moving = true;
+        }
+        else
+        {
+            dirVel.x = Vector3.Lerp(new Vector3(dirVel.x, 0, 0), Vector3.zero, Time.deltaTime * 5).x;
+            if (Mathf.Abs(m_speed) < 0.15f)
+            {
+                dirVel.x = 0;
+            }
+        }
 
-        // Apply force
+        // Action key check
+        isPressedAction = Input.GetKey(keyInputs[4]);
+
+        // Apply movement
         Vector3 pos = transform.localPosition;
         Vector3 toPos = new Vector3(
-            pos.x + (moveSpeed * dir.x),
+            pos.x + (m_speed * dirVel.x),
             pos.y,
-            pos.z + (moveSpeed * dir.z)
+            pos.z + (m_speed * dirVel.z)
         );
         Vector3 lerpPos = Vector3.Lerp(pos, toPos, (Time.deltaTime * lerpMultiplier));
         GetComponent<Rigidbody>().MovePosition(lerpPos);
@@ -64,5 +88,37 @@ public class PlayerBehavior : MonoBehaviour {
 
     private void MovementHandler()
     {
+        if (m_moving)
+        {
+            if (m_speed < moveSpeed)
+            {
+                m_speed += acceleration;
+            }
+        }
+        else
+        {
+            if (m_speed > 0)
+            {
+                m_speed -= friction;
+            }
+        }
+        
+        // Map Borders
+        if(transform.position.x >= 15)
+        {
+            transform.position = new Vector3(15, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x <= -15)
+        {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z >= 18)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 18);
+        }
+        if (transform.position.z <= -10)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        }
     }
 }
